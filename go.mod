38bbf55dd1e70b65a5ce86005f01b781f3efa6b5
Алишер Platform <@a.dautov>
2025-10-21 02:47:23 +0500

    Created User Model

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module dental_clinic
 go 1.23.4
 
 require (
+	github.com/google/uuid v1.6.0
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/lib/pq v1.10.9
 )
--- internal/models/user.go
+++ internal/models/user.go
@@ -1,1 +1,13 @@
 package models
+
+import "github.com/google/uuid"
+
+type User struct {
+	Id uuid.UUID
+	Role string
+	Email string
+	Name string
+	Gender string
+	Age int
+	Push_consent bool
+}
