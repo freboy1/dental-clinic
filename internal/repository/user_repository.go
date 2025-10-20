3aceda9267c3419d47d2919f8973de72184ee158
Алишер Platform <@a.dautov>
2025-10-21 02:55:12 +0500

    Implemented Create Method For UserRepository

--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -1,1 +1,43 @@
 package repository
+
+import (
+	"database/sql"
+	"dental_clinic/internal/models"
+)
+
+type UserRepository interface {
+	Create(user *models.User) (*models.User, error)
+	GetByID(id string) (*models.User, error)
+	Update(id string, user *models.User) error
+	Delete(id string) error
+}
+
+type userRepo struct {
+	db *sql.DB
+}
+
+func NewUserRepository(db *sql.DB) UserRepository {
+	return &userRepo{db: db}
+}
+
+func (r *userRepo) Create(user *models.User) (*models.User, error) {
+	query := `INSERT INTO users (role, email, password, name, gender, age, push_consent)
+			  VALUES ($1, $2, $3, $4, $5, $6) RETURNING id`
+	err := r.db.QueryRow(query, user.Role, user.Email, user.Password, user.Name, user.Gender, user.Age, user.Push_consent).
+		Scan(&user.Id)
+	return user, err
+}
+
+func (r *userRepo) Delete(id string) error {
+	panic("unimplemented")
+}
+
+func (r *userRepo) GetByID(id string) (*models.User, error) {
+	panic("unimplemented")
+}
+
+func (r *userRepo) Update(id string, user *models.User) error {
+	panic("unimplemented")
+}
+
+
