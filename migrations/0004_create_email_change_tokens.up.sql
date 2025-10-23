c59b8a1dbe64178e7badb035d8f537ef97a55ff2
Алишер Platform <@a.dautov>
2025-10-23 21:37:01 +0500

    Finish email_change_tokens table

---
+++ migrations/0004_create_email_change_tokens.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE email_change_tokens (
+    id SERIAL PRIMARY KEY,
+    user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+    new_email VARCHAR(255) NOT NULL,
+    token VARCHAR(255) UNIQUE NOT NULL,
+    expires_at TIMESTAMP NOT NULL DEFAULT (NOW() + INTERVAL '24 hours')
+);
