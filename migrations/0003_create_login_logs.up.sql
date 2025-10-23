dbc7b0c21664d34429c059370abaa9b7b5ccbfe6
Алишер Platform <@a.dautov>
2025-10-23 21:34:52 +0500

    Finish login_logs table

---
+++ migrations/0003_create_login_logs.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE login_logs (
+    id SERIAL PRIMARY KEY,
+    user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+    ip_address VARCHAR(45) NOT NULL,
+    login_time TIMESTAMP DEFAULT NOW(),
+    success BOOLEAN DEFAULT TRUE
+);
