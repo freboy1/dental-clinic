dde9499bde73b6a4920d7323e2acd234b2f98bb8
kamilakamilkami <nurlybayevakamila@gmail.com>
2026-03-03 21:12:34 +0800

    add doctors table

---
+++ migrations/0011_create_doctors_table.sql
@@ -0,0 +1,13 @@
+-- +goose Up
+CREATE TABLE doctors (
+                         id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                         specialization VARCHAR(255) NOT NULL,
+                         experience INT NOT NULL DEFAULT 0,
+                         clinic_id UUID REFERENCES clinics(id) ON DELETE SET NULL,
+                         bio TEXT,
+                         is_available BOOLEAN NOT NULL DEFAULT TRUE,
+                         created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+-- +goose Down
+DROP TABLE IF EXISTS doctors;
