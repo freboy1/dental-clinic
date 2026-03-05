45f9ad557efc3fd471ca02a1f1ed18f5862f37a0
kamilakamilkami <nurlybayevakamila@gmail.com>
2026-03-05 20:38:58 +0800

    add service DTO

--- internal/modules/services/dto/service_response.go
+++ internal/modules/services/dto/service_response.go
@@ -1,1 +1,35 @@
 package dto
+
+type CreateServiceRequest struct {
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Price       float64 `json:"price"`
+	Duration    int     `json:"duration"`
+	ClinicID    string  `json:"clinic_id"`
+	IsActive    bool    `json:"is_active"`
+}
+
+type UpdateServiceRequest struct {
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Price       float64 `json:"price"`
+	Duration    int     `json:"duration"`
+	ClinicID    string  `json:"clinic_id"`
+	IsActive    bool    `json:"is_active"`
+}
+
+type ServiceResponse struct {
+	Id          string  `json:"id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Price       float64 `json:"price"`
+	Duration    int     `json:"duration"`
+	ClinicID    string  `json:"clinic_id"`
+	IsActive    bool    `json:"is_active"`
+}
+
+type ServiceActionResponse struct {
+	Success   string `json:"success"`
+	Message   string `json:"message"`
+	ServiceID string `json:"service_id"`
+}
