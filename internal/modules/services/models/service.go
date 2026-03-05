cf703265c83a6db78740a20955efe57c8c79787e
kamilakamilkami <nurlybayevakamila@gmail.com>
2026-03-05 20:37:25 +0800

    add service model

--- internal/modules/services/models/service.go
+++ internal/modules/services/models/service.go
@@ -1,1 +1,13 @@
 package models
+
+import "github.com/google/uuid"
+
+type Service struct {
+	Id          uuid.UUID
+	Name        string
+	Description string
+	Price       float64
+	Duration    int
+	ClinicID    uuid.UUID
+	IsActive    bool
+}
