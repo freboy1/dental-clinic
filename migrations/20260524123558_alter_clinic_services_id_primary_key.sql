140ecac6cdc67adc5b5a35b7db409cfef21d73ba
kamilakamilkami <nurlybayevakamila@gmail.com>
2026-06-04 05:40:29 +0800

    updated migartion

--- migrations/20260524123558_alter_clinic_services_id_primary_key.sql
+++ migrations/20260524123558_alter_clinic_services_id_primary_key.sql
@@ -1,6 +1,7 @@
 -- +goose Up
-ALTER TABLE clinic_services
-    ADD PRIMARY KEY (id);
+UPDATE clinic_services SET id = gen_random_uuid() WHERE id IS NULL;
+ALTER TABLE clinic_services ALTER COLUMN id SET NOT NULL;
+ALTER TABLE clinic_services ADD PRIMARY KEY (id);
+
 -- +goose Down
-ALTER TABLE clinic_services
-DROP CONSTRAINT IF EXISTS clinic_services_pkey;
+ALTER TABLE clinic_services DROP CONSTRAINT IF EXISTS clinic_services_pkey;
